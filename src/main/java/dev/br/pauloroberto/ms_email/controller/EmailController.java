aea20525ce0a90c2cb318df76d133a040499351c
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-05 10:52:41 -0400

    Added POST route and implemented sendEmail method

--- src/main/java/dev/br/pauloroberto/ms_email/controller/EmailController.java
+++ src/main/java/dev/br/pauloroberto/ms_email/controller/EmailController.java
@@ -1,6 +1,14 @@
 package dev.br.pauloroberto.ms_email.controller;
 
+import dev.br.pauloroberto.ms_email.dto.EmailDto;
+import dev.br.pauloroberto.ms_email.model.Email;
 import dev.br.pauloroberto.ms_email.service.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +18,14 @@ public class EmailController {
 
     public EmailController(EmailService emailService) {
         this.emailService = emailService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<Email> sendEmail(@RequestBody @Valid EmailDto emailDto) {
+        Email email = new Email();
+        BeanUtils.copyProperties(emailDto, email); // Copy properties from emailDto to email if they have the same name
+        emailService.sendEmail(email);
+
+        return new ResponseEntity<>(email, HttpStatus.CREATED);
     }
 }
