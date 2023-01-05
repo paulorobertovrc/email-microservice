4fb1d3040565e35b98be9de872845b04576d33c3
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-05 10:50:25 -0400

    This service encapulates the JavaMailSender instance

---
+++ src/main/java/dev/br/pauloroberto/ms_email/service/EmailSenderService.java
@@ -0,0 +1,18 @@
+package dev.br.pauloroberto.ms_email.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+    private final JavaMailSender mailSender;
+
+    public EmailSenderService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(SimpleMailMessage message) {
+        mailSender.send(message);
+    }
+}
