82d7616e2a4ee3f5085ceb47f9150d825f9ad156
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-06 09:56:46 -0400

    Refactored and added list methods

--- src/main/java/dev/br/pauloroberto/ms_email/service/EmailService.java
+++ src/main/java/dev/br/pauloroberto/ms_email/service/EmailService.java
@@ -1,9 +1,13 @@
 package dev.br.pauloroberto.ms_email.service;
 
+import dev.br.pauloroberto.ms_email.dto.EmailDto;
 import dev.br.pauloroberto.ms_email.model.Email;
 import dev.br.pauloroberto.ms_email.model.StatusEmail;
 import dev.br.pauloroberto.ms_email.repository.EmailRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -12,17 +16,24 @@ import java.time.LocalDateTime;
 public class EmailService {
 
     private final EmailRepository emailRepository;
-    private final EmailSenderService emailSenderService;
+    private final JavaMailSender javaMailSender;
 
-    public EmailService(EmailRepository emailRepository, EmailSenderService emailSenderService) {
+    public EmailService(EmailRepository emailRepository,JavaMailSender javaMailSender) {
         this.emailRepository = emailRepository;
-        this.emailSenderService = emailSenderService;
+        this.javaMailSender = javaMailSender;
+    }
+
+    public Email createEmail(EmailDto emailDto) {
+        Email email = new Email();
+
+        email.setSendDateEmail(LocalDateTime.now());
+        BeanUtils.copyProperties(emailDto, email);
+
+        return email;
     }
 
     public void sendEmail(Email email) {
-        email.setSendDateEmail(LocalDateTime.now());
-
-        try { // Try to send the email
+        try {
             SimpleMailMessage message = new SimpleMailMessage();
 
             message.setFrom(email.getEmailFrom());
@@ -30,13 +41,21 @@ public class EmailService {
             message.setSubject(email.getSubject());
             message.setText(email.getText());
 
-            emailSenderService.sendEmail(message);
+            javaMailSender.send(message);
 
             email.setStatusEmail(StatusEmail.SENT);
-        } catch (Exception e) { // If an error occurs, set the status to ERROR
+        } catch (Exception e) {
             email.setStatusEmail(StatusEmail.ERROR);
-        } finally { // Save the email in the database
+        } finally {
             emailRepository.save(email);
         }
+    }
+
+    public Iterable<Email> list(Pageable pageable) {
+        return emailRepository.findAll(pageable);
+    }
+
+    public Email list(Long id) {
+        return emailRepository.findById(id).orElseThrow();
     }
 }
